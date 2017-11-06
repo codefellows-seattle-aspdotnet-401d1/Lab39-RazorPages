5d15744019b9ec420f66a00f1e6d0056a3f733a4
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-06 11:13:52 -0800

    scaffolded movie model

---
+++ Lab39Tom/Lab39Tom/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lab39Tom.Models;
+
+namespace Lab39Tom.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Lab39Tom.Models.MovieContext _context;
+
+        public IndexModel(Lab39Tom.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}

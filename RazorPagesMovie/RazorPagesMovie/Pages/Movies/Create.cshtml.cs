00811acec29fc80c00d0a16999c244742321f86e
miyaluc <srlucas3@gmail.com>
2017-11-06 16:25:03 -0800

    added SeedData class

---
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie.Models.MovieContext _context;
+
+        public CreateModel(RazorPagesMovie.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        //this method initializes any state needed for the page
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        //this bind property allows the movie property to opt-in to model binding
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

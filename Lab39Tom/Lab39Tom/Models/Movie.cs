45492b240cfb69416817ac90c3efc75ce52ddbc3
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-08 11:13:52 -0800

    added data annotations and comments to Movie.cs

--- Lab39Tom/Lab39Tom/Models/Movie.cs
+++ Lab39Tom/Lab39Tom/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,11 @@ namespace Lab39Tom.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        //specifies how field name is displayed
+        [Display(Name = "Release Date")]
+        //specifies DataType of Date, whic removes time from DateTime
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }

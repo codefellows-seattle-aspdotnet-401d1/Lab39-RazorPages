fa2f44de5f474e1e04aab26750a5ed14eb9ae890
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-08 11:46:55 -0800

    added validation to Movie model

--- Lab39Tom/Lab39Tom/Models/Movie.cs
+++ Lab39Tom/Lab39Tom/Models/Movie.cs
@@ -9,6 +9,8 @@ namespace Lab39Tom.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required, StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         //specifies how field name is displayed
@@ -16,8 +18,14 @@ namespace Lab39Tom.Models
         //specifies DataType of Date, whic removes time from DateTime
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
